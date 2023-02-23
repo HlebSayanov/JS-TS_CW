4a16cf30d0b7d976d58009acfef6fd7a1e6fc082
HlebSayanov <say4havi@gmail.com>
2023-02-23 23:57:20 +0300

    add_task_CW

--- src/udemi_course/main.js
+++ src/udemi_course/main.js
@@ -114,3 +114,136 @@ supporter1.chant()
 supporter2.chant= supporter1.chant.bind(supporter2)
 supporter2.chant()
 */
+
+//==========================================================-------------------------------------
+
+// Проект. Практика
+// Создайте функцию censor, которая не принимает аргументов. Функция должна возвращать новую функцию,
+//     принимающую опционально 1 или 2 параметра (оба - строки).
+//     Когда возвращаемая функция принимает 2 параметра, то она ничего не возвращает,
+//     а сохраняет полученные значения как пару (пара - это массив с двумя значениями).
+// Когда возвращаемая функция принимает 1 параметр, то она возвращает полученную строку,
+//     заменив в ней все слова, согласно ранее сохранённым парам.
+//
+//     Пример использования:
+//
+//  const changeScene = censor();
+// changeScene('PHP', 'JS');
+// changeScene('backend', 'frontend');
+// console.log(changeScene('PHP is the most popular programming language for backend web-development'));
+// должно распечатать 'JS is the most popular programming language for frontend web-development'
+//
+// Примечание: в переданной строке, в качестве единственного параметра,
+//     замена происходит согласно парам, где первое значение пары - что мы хотим заменить,
+//     второе значение пары - на что хотим заменить.
+//
+// function censor() {
+//     const newFanc =(a,b)=>{
+//         if(a && b){
+//             return
+//         }else{
+//
+//         }
+//     }
+//     return newFanc
+// }
+
+//===============================================================================================================
+
+const numbers = [1, 22, 12, 66, 90, 51, 11];
+const doubledNumbers = numbers.map(el=>el*2)
+
+const developers = [
+    {
+        id: 1,
+        fullName: 'Anton Petrov',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Git', 'React'],
+        salary: 1000,
+    },
+    {
+        id: 2,
+        fullName: 'Ivan Ivanov',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Git', 'Vue'],
+        salary: 950,
+    },
+    {
+        id: 3,
+        fullName: 'Albert Sidorov',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Git', 'jQuery'],
+        salary: 850,
+    },
+];
+
+const middleDevelopers = developers.map(el=> {
+    el.salary +=500
+    el.skills =[...el.skills,'TypeScript']
+    return el
+} )
+
+
+console.log(middleDevelopers)
+
+// Практика на reduce
+// Дана коллекция товаров в корзине order. У каждого товара есть цена price и количество quantity.
+//     Подсчитайте общую стоимость товаров в корзине и сохраните значение в переменной totalPrice.
+
+const order = [
+    {
+        id: 1,
+        name: 'Лопата',
+        price: 1000,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        name: 'Удочка',
+        price: 1200,
+        quantity: 2,
+    },
+    {
+        id: 3,
+        name: 'Ведро',
+        price: 500,
+        quantity: 3,
+    },
+    {
+        id: 4,
+        name: 'Мороженое',
+        price: 100,
+        quantity: 8,
+    },
+];
+
+const totalPrice = order.reduce((acc, sum)=>{
+   return acc +sum.price*sum.quantity
+},0)
+
+console.log(totalPrice)
+const players = [
+    {
+        id: 1,
+        name: 'Andres',
+        surname: 'Iniesta',
+        club: 'Vissel Cobe',
+    },
+    {
+        id: 2,
+        name: 'Eden',
+        surname: 'Hazard',
+        club: 'Real Madrid',
+    },
+    {
+        id: 3,
+        name: 'Mo',
+        surname: 'Salah',
+        club: 'Liverpool',
+    },
+    {
+        id: 4,
+        name: 'Lionel',
+        surname: 'Messi',
+        club: 'Barcelona',
+    },
+]
+const messi = players.find(el=> el.surname === 'Messi')
+console.log(messi)
